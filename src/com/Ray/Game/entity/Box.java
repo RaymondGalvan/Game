9b685023b4f5afb844c0f3781768c0086068b43f
RaymondGalvan <trollmaster6699@gmail.com>
2015-06-25 11:39:15 -0700

    edited original thing

---
+++ src/com/Ray/Game/entity/Box.java
@@ -0,0 +1,29 @@
+package com.Ray.Game.entity;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Point;
+
+public class Box extends BaseEntity {
+	
+	public Box(Point point, int i, int j, int k) {
+		super(point, i, j, k);
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)throws SlickException {
+		position.setX(position.getX()+ 1);
+		position.setY(position.getY()+ 1);
+		
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		g.setColor(Color.red);
+		g.drawRect(position.getX(), position.getY(), width, height);
+
+	}
+
+}
