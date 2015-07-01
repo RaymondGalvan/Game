33295a60664042c5c9eabf39f4f88fe8af5794ec
RaymondGalvan <trollmaster6699@gmail.com>
2015-07-01 09:04:43 -0700

    ughu

---
+++ src/com/Raymond/entities/Pacman.java
@@ -0,0 +1,29 @@
+package com.Raymond.entities;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
+
+public class Pacman extends BaseEntity {
+	
+private Circle Pacman;
+	public Pacman(int zIndex) {
+		super(zIndex);
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		
+
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		
+
+	}
+
+}
